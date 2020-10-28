0b6fb8ea2c808e8635cfd0567c0b7d06d5439077
Jeffrey Jones <jeff@jones.be>
2020-10-28 20:50:12 +0900

    Add Airfield ApproachController
    Add an Airfield that returns the route of waypoints a pilot should
    follow to get to the closest active runway.

---
+++ RurouniJones-DCS-Airfields-Tests/ApproachController/InboundToRunwayTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Geo.Geometries;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RurouniJones.DCS.Airfields.Controllers.Approach;
+
+namespace RurouniJones.DCS.Airfields.Controllers.Tests
+{
+    [TestClass]
+    public class InboundToRunwayTests
+    {
+        public static IEnumerable<object[]> ApproachScenarios()
+        {
+            yield return new object[] { new ApproachScenario("Anapa-Vityazevo", 090, new Point(44.0101581, 37.3481765), new List<string>{"Entry South", "Initial-0 4", "Runway-0 4"}) };
+            yield return new object[] { new ApproachScenario("Anapa-Vityazevo", 090, new Point(46.0101581, 37.3481765), new List<string>{"Entry-Exit West", "Initial-0 4", "Runway-0 4"}) };
+            //yield return new object[] { new ApproachScenario("Anapa-Vityazevo", 270, new Point(44.0101581, 37.3481765), new List<string>{"Entry-Exit East", "Initial-2 2", "Runway-2 2"}) };
+            //yield return new object[] { new ApproachScenario("Anapa-Vityazevo", 270, new Point(46.0101581, 37.3481765), new List<string>{"Entry-Exit North", "Initial-2 2", "Runway-2 2"}) };
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(ApproachScenarios), DynamicDataSourceType.Method)]
+        public void GetApproachRoute(ApproachScenario scenario)
+        {
+            var airfield = Populator.Airfields.First(af => af.Name.Equals(scenario.Airfield));
+            var controller = new ApproachController(airfield);
+
+            airfield.WindHeading = scenario.Wind;
+
+            var expected = scenario.Waypoints;
+
+            var actual = controller.GetApproachRoute(scenario.StartPoint).Select(x => x.Name).ToList();
+
+            Console.WriteLine(string.Join(" -> ", actual));
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        public class ApproachScenario
+        {
+            public string Airfield { get; }
+            public int Wind { get; }
+            public Point StartPoint { get; }
+            public List<string> Waypoints;
+
+            public ApproachScenario(string airfield, int wind, Point startPoint, List<string> wayPoints)
+            {
+                Airfield = airfield;
+                Wind = wind;
+                StartPoint = startPoint;
+                Waypoints = wayPoints;
+            }
+            
+            public override string ToString()
+            {
+                return $"{Airfield} (Wind {Wind:D3}): {Waypoints.First()} -> {Waypoints.Last()}";
+            }
+        }
+    }
+}
--- RurouniJones-DCS-Airfields/Controllers/Approach/ApproachController.cs
+++ RurouniJones-DCS-Airfields/Controllers/Approach/ApproachController.cs
@@ -1,4 +1,10 @@
-﻿using RurouniJones.DCS.Airfields.Structure;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Geo.Geometries;
+using QuikGraph;
+using QuikGraph.Algorithms;
+using RurouniJones.DCS.Airfields.Controllers.Util;
+using RurouniJones.DCS.Airfields.Structure;
 
 namespace RurouniJones.DCS.Airfields.Controllers.Approach
 {
@@ -6,6 +12,54 @@ namespace RurouniJones.DCS.Airfields.Controllers.Approach
     {
         public ApproachController(Airfield airfield) : base(airfield) {}
 
-        public void GetApproachRoute() {}
+        public List<NavigationPoint> GetApproachRoute(Point callerPosition)
+        {
+            var paths = GetPathsToActiveRunways();
+            var taggedEdges = paths.OrderBy(x => x[0].Source.DistanceTo(callerPosition.Coordinate)).First();
+            var navigationPoints = taggedEdges.Select(edge => edge.Source).ToList();
+            // Add the runway itself
+            navigationPoints.Add(taggedEdges.Last().Target);
+            return navigationPoints;
+        }
+
+        private List<List<TaggedEdge<NavigationPoint, string>>> GetPathsToActiveRunways()
+        {
+            // First we get a list of active runway(s)
+            var activeRunways = ActiveRunwayDecider.GetActiveRunways(_airfield);
+
+            // Then we get a list of all the entry Waypoints
+            var entryPoints = _airfield.WayPoints.Where(x => x.Name.ToLower().Contains("entry"));
+
+            var paths = new List<KeyValuePair<double, List<TaggedEdge<NavigationPoint,string>>>>();
+
+            // Get all the paths from all the entry waypoints to the active runway(s)
+            foreach (var entryPoint in entryPoints)
+            {
+                var tryGetPaths =
+                    _airfield.NavigationGraph.ShortestPathsDijkstra(_airfield.NavigationCostFunction, entryPoint);
+
+                foreach (var runway in activeRunways)
+                {
+                    if (!tryGetPaths(runway, out var path)) continue;
+                    var taggedEdges = path.ToList();
+                    var pathCost = PathCost(taggedEdges);
+                    var pathWithCost = new KeyValuePair<double, List<TaggedEdge<NavigationPoint,string>>>(pathCost, taggedEdges);
+                    paths.Add(pathWithCost);
+                }
+            }
+
+            // Sort so that we get the cheapest ones first
+            var cheapestPathByCost = paths.OrderBy(pair => pair.Key).ToList();
+
+            // This is the cheapest one, so we want this one and any that have the same cost
+            // since there could be multiple entry points for a given active runway(s).
+            var cheapestCost = paths[0].Key;
+
+            cheapestPathByCost.RemoveAll(pair => pair.Key > cheapestCost);
+
+            // And finally return the paths so the caller can find the one with the closest
+            // start point to the player, which will be the actual approach path.
+            return cheapestPathByCost.Select(pathPair => pathPair.Value).ToList();
+        }
     }
 }
